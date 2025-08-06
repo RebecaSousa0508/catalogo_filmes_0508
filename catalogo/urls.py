395616c75d724b1bd45d7d7429db00f846ade9b1
RebecaSousa0508 <rebecavarnesousaassuncao@gmail.com>
2025-08-06 11:20:13 -0300

    atualizações 06/08
    novas templates, cadastro e login

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 #from . import views 
-from .views import home, meus_filmes
+from .views import home, login, cadastro
 
 urlpatterns = [
     path('', home, name='home'),
-    path('meus_filmes', meus_filmes, name='meus_filmes'), 
-    
+    path('login/', login, name='login'),
+    path('cadastro/', cadastro, name='cadastro'),
+     
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,9 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import CadastroForm
 
 # Create your views here.
 def home(request):
     return render(request, 'catalogo/home.html') 
 
-def meus_filmes(request):
-    return render(request, 'catalogo/meus_filmes.html')
+def login(request):
+    return render(request, 'catalogo/login.html')
+
+def cadastro(request):
+    if request.method != 'POST':
+        form = CadastroForm()
+    else:
+        form = CadastroForm(request.POSTS)
+        if form.is_valid():
+            if request.POST['password'] == request.POST['confirm_password']:
+                user = User.objects.create_user(username=form.cleaned_data ['username'], email= form.cleaned_data['email'], password = form.cleaned_data['password']) 
+                perfil = Perfil.objects.create(user= user, tipo_usuario= form.cleaned_data['tipo_usuario'])
+                user.save()
+                perfil.save()
+                messages.sucess(request, 'Cadastro realizado com sucesso!')
+                return redirect ('home')
+            else:
+                messages.error(request, 'As senhas não conferem')
+            
+
+    context = {'form':form}
+    return render(request, 'catalogo/cadastro.html')
+
+
